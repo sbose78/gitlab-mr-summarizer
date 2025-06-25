23ebcab4c0464f96204991592d4e5c7c97a22dc2
Shoubhik Bose <shbose@redhat.com>
2025-06-25 02:26:43 -0400

    fix: race condition (#9)

--- framework/context/git.py
+++ framework/context/git.py
@@ -2,11 +2,13 @@ from framework.interfaces import ContextGenerator
 import tempfile
 import subprocess
 import os
+import uuid
 
 class GitCloneContextGenerator(ContextGenerator):
     def __init__(self, repo_url, branch=None):
         self.repo_url = repo_url
         self.branch = branch
+        self.temp_dir = None
 
     def requires(self):
         return set()
@@ -17,7 +19,12 @@ class GitCloneContextGenerator(ContextGenerator):
     def generate(self, context):
         if 'repo_dir' in context:
             return  # Already cloned
-        temp_dir = tempfile.mkdtemp(prefix="repo_")
+        
+        # Create unique temp directory with UUID to avoid conflicts
+        unique_id = str(uuid.uuid4())[:8]
+        temp_dir = tempfile.mkdtemp(prefix=f"repo_{unique_id}_")
+        self.temp_dir = temp_dir
+        
         clone_cmd = ["git", "clone", self.repo_url, temp_dir]
         if self.branch:
             clone_cmd += ["-b", self.branch]
@@ -27,4 +34,5 @@ class GitCloneContextGenerator(ContextGenerator):
     def cleanup(self):
         if self.temp_dir and os.path.exists(self.temp_dir):
             import shutil
-            shutil.rmtree(self.temp_dir) 
+            shutil.rmtree(self.temp_dir)
+            self.temp_dir = None 
